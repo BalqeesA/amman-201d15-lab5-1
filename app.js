a3bf059bc0bc9ca64bc2ea5f9c55633c56427048
BalqeesA <basbosared@ymail.com>
2021-02-13 21:33:30 +0200

    finished problem 5

--- app.js
+++ app.js
@@ -79,14 +79,16 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 let testArray = [2, 3, 4]; //eslint-disable-line
-
 function sumArray(sumArr) {
   //eslint-disable-line
+  for (let i=0; i< len; i++)
+  sum  += sumArr(testArray[i]);
+  return =( testArray , `2,3,4 was passed in as an array of numbers, and 9 is their sum `)
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -99,14 +101,20 @@ Write a function called multiplyArray() that takes an array of numbers as its ar
 IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this. This function should handle an array containing three elements. However, you may continue to use the + operator for string concatenation.
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
-
+let testArray = [2, 3, 4];
 // Write your code here
 function multiplyArray(multArr) {
   //eslint-disable-line
+  for (i = testArray; i <= multArr; i++) {
+    if(i < multArr){
+      text += i + " * ";
+    }else{
+      text += i;
+    }
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop.
 
